7c220c556baeb9b14c107177367b46e5f70d659b
fofcn <errorfatal89@gmail.com>
2022-04-09 22:38:48 +0800

    1、增加web工程中spring boot依赖 2、增加shardingsphere

---
+++ web/src/main/java/org/fofcn/trivialfs/web/TrivialFsWebSpringBootMain.java
@@ -0,0 +1,12 @@
+package org.fofcn.trivialfs.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrivialFsWebSpringBootMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrivialFsWebSpringBootMain.class, args);
+    }
+}
